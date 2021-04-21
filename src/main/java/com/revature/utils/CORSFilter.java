5da504397261ea2545ba452dd40dcb6a7c928427
Mitchell <mitchellfrehr@gmail.com>
2021-04-21 18:12:51 -0400

    created initial frontend architecture and CORS filter to bypass browser security

---
+++ src/main/java/com/revature/utils/CORSFilter.java
@@ -0,0 +1,35 @@
+
+public class CORSFilter implements Filter {
+	
+	   @Override
+	    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	            throws IOException, ServletException {
+	        
+	        System.out.println("CORS Filter leveraged");
+	        
+	        if(!(response instanceof HttpServletResponse)) {
+	            chain.doFilter(request, response);
+	            return;
+	        }
+	        
+	        // Cast the response as an HttpServletResponse
+	        // Which is important, because we are going to set
+	        // headers, which is specific to HTTP
+	        HttpServletResponse res = (HttpServletResponse) response;
+	        
+	        res.setHeader("Access-Control-Allow-Origin", "null"); // Allow all origins
+	        
+	        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	        // Allow specific HTTP Verbs
+	        
+	        res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type,"
+	                + "Access-Control-Request-Method, Access-Control-Request-Headers");
+	        // Allow specific HTTP Headers (there's a fair few)
+	        
+	        res.setHeader("Access-Control-Allow-Credentials", "true");
+	        // Credentials are allowed
+	        
+	        chain.doFilter(request, response);
+	        // Continue the filter chain
+	    }
+}
