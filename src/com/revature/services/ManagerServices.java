449d54e1ac03aecbb0af28c30a21f15a3117e468
Mitchell <mitchellfrehr@gmail.com>
2021-04-27 18:33:30 -0400

    created service methods to interact with our DAO

--- src/com/revature/services/ManagerServices.java
+++ src/com/revature/services/ManagerServices.java
@@ -1,5 +1,31 @@
 package com.revature.services;
 
-public class ManagerServices {
+import java.util.List;
 
-}
+import com.revature.models.Managers;
+import com.revature.repos.ManagerDAOsImpl;
+
+public class ManagerServices {
+		
+		private static ManagerDAOsImpl mDao = new ManagerDAOsImpl();
+		
+		public boolean login(Managers user) {
+			Managers userFromDB = mDao.getUserByUsername(user.getUsername());
+			if(userFromDB!=null && userFromDB.getPassword().equals(user.getPassword())) {
+				return true;
+			}
+			return false;
+		}
+
+		public List<Managers> getPendingRequests() {
+			return mDao.pendingRequests();
+		}
+		
+		public List<Managers> getCompletedRequests() {
+			return mDao.completedRequests();
+		}
+		
+	}
+
+
+	
