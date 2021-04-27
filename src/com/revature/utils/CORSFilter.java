5caf076b6070bb11fc07a265aa7cfc0fc52f2ebd
Mitchell <mitchellfrehr@gmail.com>
2021-04-27 18:32:24 -0400

    updated CORSFilter with correct imports

--- src/com/revature/utils/CORSFilter.java
+++ src/com/revature/utils/CORSFilter.java
@@ -2,6 +2,13 @@ package com.revature.utils;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 public class CORSFilter implements Filter {
 	
 	   @Override
